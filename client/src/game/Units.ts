0167b8b93cefd7773e13668747fc86abc5a64110
alexander813 <molokoflina@gmail.com>
2025-09-17 18:33:41 +0300

    new file:   client/src/assets/img/sprites/defaultUnit.png 	new file:   client/src/game/Units.ts

---
+++ client/src/game/Units.ts
@@ -0,0 +1,26 @@
+// Базовый класс Unit
+export class Unit {
+    sprite: string = ''; // путь к PNG файлу спрайта
+    hp: number | undefined;
+    damage: number | undefined;
+    x: number = 0; // позиция в матрице
+    y: number = 0; // позиция в матрице
+    
+    constructor(x: number = 0, y: number = 0, sprite: string = '', hp?: number, damage?: number) {
+        this.x = x;
+        this.y = y;
+        this.sprite = sprite;
+        this.hp = hp;
+        this.damage = damage;
+    }
+}
+
+// Наследный класс DefaultUnit с предустановленной текстурой
+export class DefaultUnit extends Unit {
+    constructor(x: number = 0, y: number = 0, hp: number = 100, damage: number = 25) {
+        super(x, y, './assets/img/sprites/defaultUnit.png', hp, damage);
+    }
+}
+
+// Экспортируем DefaultUnit как default для обратной совместимости
+export default DefaultUnit;
