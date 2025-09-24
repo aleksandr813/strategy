706c500039746f3b340a55929354d6c04758492b
Kasatik777 <temakasatka2000@gmail.com>
2025-09-24 21:10:33 +0400

    modified:   client/src/game/Game.ts

--- client/src/game/Game.ts
+++ client/src/game/Game.ts
@@ -5,12 +5,17 @@ const { WIDTH, HEIGHT } = CONFIG;
 
 class Game {
     private units:Unit[];
-    private builds:Build[]
+    private builds:Build[];
+    binarMatrix:number[];
+    
 
     constructor() {
         this.units = [new Unit(0, 0), new Unit(0, 5)]
         this.builds = [new Build(5, 5)]
+        this.binarMatrix = []
+        
     }
+    
 
     destructor() {
         //...
@@ -22,6 +27,24 @@ class Game {
             builds: this.builds,
         };
     }
+
+    matrixForEasyStar () {
+        for (let i = 0; i < 29 * 29; i++) {
+            this.binarMatrix.push(0); // бинарная матрица заполняется нулями
+            const x = Math.floor(i / 29);//
+            const y = i % 29;//уловные х и у для бинарной матрицы
+            if ((this.units[i].cords.x = x) && (this.units[i].cords.y = y)) { //проверка на юнитов: координаты юнита(х, у) сравниваются с условными координатами бинарной матрицы
+                this.binarMatrix[i] = 1;
+            }
+            for (let j = 0; j < 3; j ++) {
+                if ((this.builds[i].cords[j].x = x) && (this.builds[i].cords[j].y = y)) { //проверка на здания
+                    this.binarMatrix[i] = 1;
+                }
+            }
+        }
+    } 
+
+    
 
     /**
     move(dx: number, dy: number): void {
