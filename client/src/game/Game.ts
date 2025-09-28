7987b12fe9ecfd2a4e7c87816ce7bfb898f2271c
aleksandr813 <molokoflina@gmail.com>
2025-09-28 16:05:36 +0400

    modified:   client/src/game/Game.ts

--- client/src/game/Game.ts
+++ client/src/game/Game.ts
@@ -6,13 +6,15 @@ const { WIDTH, HEIGHT } = CONFIG;
 class Game {
     private units:Unit[];
     private builds:Build[];
-    binarMatrix:number[];
+    binaryMatrix:number[];
+    private villageMatrix:boolean[];
     
 
     constructor() {
         this.units = [new Unit(0, 0), new Unit(0, 5)]
         this.builds = [new Build(5, 5)]
-        this.binarMatrix = []
+        this.binaryMatrix = []
+        this.villageMatrix = [];
         
     }
     
@@ -28,26 +30,13 @@ class Game {
         };
     }
 
-    matrixForEasyStar () {
-        for (let i = 0; i < 29 * 29; i++) {
-            this.binarMatrix.push(0); // бинарная матрица заполняется нулями
-            const x = Math.floor(i / 29);//
-            const y = i % 29;//уловные х и у для бинарной матрицы
-            for (let j = 0; j < this.units.length; j++) {
-                if ((this.units[j].cords.x = x) && (this.units[j].cords.y = y)) { //проверка на юнитов: координаты юнита(х, у) сравниваются с условными координатами бинарной матрицы
-                    this.binarMatrix[i] = 1;
-                }
-            }
-            for (let j = 0; j < this.builds.length; j++) {
-                if ((this.builds[j].cords[0].x = x) && (this.builds[j].cords[0].y = y)) { //проверка на здания. Проверяется только нижний левый угол. В оставшиесе клетки тоже передаётся 1
-                    this.binarMatrix[i] = 1;
-                    this.binarMatrix[i + 1] = 1;
-                    this.binarMatrix[i - 29] = 1;
-                    this.binarMatrix[i - 28] = 1;
-                }
-            }
+    getVillageMatrix(units:Unit[], builds:Build[]):boolean[][] {
+        let booleanMatrix:boolean[][] = new Array(29);
+        for (let i = 0; i < 29; i++) {
+            booleanMatrix[i] = new Array(29).fill(0);
         }
-    } 
+        return booleanMatrix;
+    }
 
     
 
