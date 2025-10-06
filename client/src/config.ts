debdc78d79f93f4e9c5da4cc4275608d70617fe9
golpgelop <golpgelop@gmail.com>
2025-10-06 13:07:56 +0400

    modified:   client/src/config.ts

--- client/src/config.ts
+++ client/src/config.ts
@@ -19,7 +19,8 @@ export type TWINDOW = {
 
 const CONFIG = {
     //HOST: 'http://localhost/',
-    HOST: 'http://nopainnogame/api/index.php',
+    HOST: 'http://nopainnogame.local/api',
+    //HOST: 'http://nopainnogame/api/index.php',
 
     CHAT_TIMESTAMP: 200, //ms
 
