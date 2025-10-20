e77cc08e38e5ed92977d24c06da4c2442383f15a
golpgelop <137231640+golpgelop@users.noreply.github.com>
2025-10-20 13:28:55 +0300

    35 Закончил с регистрацией полность. (#56)
    * modified:   client/src/components/Button/Button.tsx
    	modified:   client/src/config.ts
    	modified:   client/src/pages/Battle/Battle.tsx
    	modified:   client/src/pages/Calculator/Calculator.tsx
    	modified:   client/src/pages/Chat/Chat.tsx
    	modified:   client/src/pages/GlobalMap/GlobalMap.tsx
    	modified:   client/src/pages/Login/Login.tsx
    	modified:   client/src/pages/PageManager.tsx
    	modified:   client/src/pages/Registration/Registration.tsx
    	modified:   client/src/pages/Village/Village.tsx
    	deleted:    package-lock.json
    	deleted:    package.json
    	modified:   server/api/application/user/User.php
    
    * new file:   client/src/assets/img/sprites/arrow.png
    	new file:   client/src/assets/img/sprites/spearman.png
    	new file:   client/src/assets/img/sprites/archer.png
    	new file:   client/src/assets/img/sprites/swordman.png
    	modified:   client/src/pages/Registration/Registration.scss
    	modified:   client/src/pages/Registration/Registration.tsx
    	modified:   server/api/application/Answer.php
    
    * Сделал красивое оформление, добавил чекбокс ЗАПОМНИТЬ МЕНЯ

--- client/src/pages/Registration/Registration.tsx
+++ client/src/pages/Registration/Registration.tsx
@@ -2,61 +2,88 @@ import React, { useContext, useRef, useState } from 'react';
 import { ServerContext } from '../../App';
 import Button from '../../components/Button/Button';
 import { IBasePage, PAGES } from '../PageManager';
-
 import './Registration.scss';
 
-const Registration: React.FC<IBasePage> = (props: IBasePage) => {
-    const { setPage } = props;
-    const server = useContext(ServerContext);
-    const loginRef = useRef<HTMLInputElement>(null);
-    const passwordRef = useRef<HTMLInputElement>(null);
-    const confirmPasswordRef = useRef<HTMLInputElement>(null);
-    const nameRef = useRef<HTMLInputElement>(null);
-    const [passwordError, setPasswordError] = useState('');
+import arrow from '../../assets/img/sprites/arrow.png';
 
-    const registerClickHandler = async () => {
-        if (loginRef.current && passwordRef.current && confirmPasswordRef.current && nameRef.current) {
-            const login = loginRef.current.value;
-            const password = passwordRef.current.value;
-            const confirmPassword = confirmPasswordRef.current.value;
-            const name = nameRef.current.value;
-            
-            // Проверка совпадения паролей
-            if (password !== confirmPassword) {
-                setPasswordError('Пароли не совпадают');
-                return;
-            }
-            
-            setPasswordError(''); // Сбрасываем ошибку если пароли совпадают
-            
-            if (login && password && name && await server.registration(login, password, name)) {
-                setPage(PAGES.CHAT);
-            }
+import swordman from '../../assets/img/sprites/swordman.png';
+import archer from '../../assets/img/sprites/archer.png';
+import spearman from '../../assets/img/sprites/spearman.png';
+
+const Registration: React.FC<IBasePage> = ({ setPage }) => {
+const server = useContext(ServerContext);
+const loginRef = useRef<HTMLInputElement>(null);
+const passwordRef = useRef<HTMLInputElement>(null);
+const confirmPasswordRef = useRef<HTMLInputElement>(null);
+const nameRef = useRef<HTMLInputElement>(null);
+const [passwordError, setPasswordError] = useState('');
+
+
+const registerClickHandler = async () => {
+    if (loginRef.current && passwordRef.current && confirmPasswordRef.current && nameRef.current) {
+        const login = loginRef.current.value;
+        const password = passwordRef.current.value;
+        const confirmPassword = confirmPasswordRef.current.value;
+        const name = nameRef.current.value;
+
+        if (password !== confirmPassword) {
+            setPasswordError('Пароли не совпадают');
+            return;
+        }
+
+        setPasswordError('');
+
+        if (login && password && name && await server.registration(login, password, name)) {
+            setPage(PAGES.CHAT);
         }
     }
+};
 
-    const backClickHandler = () => setPage(PAGES.LOGIN);
+const backClickHandler = () => setPage(PAGES.LOGIN);
 
-    return (<div className='registration'>
-        <h1 id='title'>STRATEGY</h1>
-        <div className='registration-wrapper'>
-            <div className='registration-inputs'>
-                <p>Логин</p>
-                <input ref={loginRef} id="Test-input-login"/>
-                <p>Пароль</p>
-                <input ref={passwordRef} type='password' id="Test-input-password"/>
-                <p>Подтверждение</p>
-                <input ref={confirmPasswordRef} type='password' id="Test-input-password-2"/>
-                {passwordError && <p style={{color: 'red', fontSize: 'smaller'}}>{passwordError}</p>}
-                <p>Отображаемое имя</p>
-                <input ref={nameRef} id="Test-input-name"/>
-            </div>
-            <div className='registration-buttons'>
-                <Button onClick={registerClickHandler} text='Зарегистрироваться' id="Test-button-registration"/>
-                <Button onClick={backClickHandler} text='Назад' id="Test-button-back"/>
+return (
+    <div className="registration">
+        <div className="background-characters left"></div>
+        <div className="background-characters right"></div>
+
+        <img src={arrow} className="arrow arrow-left" />
+        <img src={arrow} className="arrow arrow-right" />
+
+        <img src={swordman} className="swordman swordman-left" />
+        <img src={archer} className="archer archer-left" />
+        <img src={spearman} className="spearman spearman-left" />
+
+        <img src={swordman} className="swordman swordman-right" />
+        <img src={archer} className="archer archer-right" />
+        <img src={spearman} className="spearman spearman-right" />
+    
+
+        <div className="registration-content">
+            <h1 className='version'>Alpha</h1>
+            <h1 className="title">STRATEGY</h1>
+
+            <div className="registration-form">
+                <label>Логин</label>
+                <input ref={loginRef} id="Test-input-login" />
+
+                <label>Пароль</label>
+                <input ref={passwordRef} type="password" id="Test-input-password" />
+
+                <label>Подтверждение пароля</label>
+                <input ref={confirmPasswordRef} type="password" id="Test-input-password-2" />
+                {passwordError && <p className="error">{passwordError}</p>}
+
+                <label>Отображаемое имя</label>
+                <input ref={nameRef} id="Test-input-name" />
+
+                <Button onClick={registerClickHandler} text="Зарегистрироваться" id="Test-button-registration" />
+                <Button onClick={backClickHandler} text="Назад" id="Test-button-back" />
             </div>
         </div>
-    </div>)
-}
+    </div>
+);
+
+
+};
 
 export default Registration;
