a067178109f7db39213da81313e2c68b38e30b94
aleksander813 <molokoflina@gmail.com>
2025-12-04 19:58:01 +0400

    modified:   client/src/hooks/useStore.ts 	new file:   client/src/services/Mediator/Mediator.ts

--- client/src/hooks/useStore.ts
+++ client/src/hooks/useStore.ts
@@ -1,20 +1,20 @@
-import { useEffect, useState } from 'react';
-import Store from '../services/store/Store';
+import Storage from '../services/store/Store';
+import { TSTORE } from '../config';
+import Mediator from '../services/Mediator/Mediator';
 
-export function useStoreMoney(store: Store) {
-    const [money, setMoney] = useState(store.getMoney());
-
-    useEffect(() => {
-        const updateMoney = () => {
-            setMoney(store.getMoney());
-        };
-
-        const unsubscribe = store.subscribe(updateMoney);
-        
-        return () => {
-            unsubscribe();
-        };
-    }, []);
-
-    return money;
+type TUseStorage = {
+    STORAGE: TSTORE,
+    mediator: Mediator,
 }
+
+const useStore = (options: TUseStorage) => {
+    const { mediator, STORAGE } = options;
+    const storage = new Storage(STORE);
+    const { GET_STORAGE, SET_STORAGE, CLEAR_STORAGE } = mediator.getTriggerTypes();
+
+    mediator.set(GET_STORAGE, (NAME) => NAME?.name ? storage.get(NAME.name) : null);
+    mediator.set(SET_STORAGE, ({ NAME, value }) => NAME?.name && storage.set(NAME.name, value));
+    mediator.set(CLEAR_STORAGE, (NAME) => NAME?.name && storage.clear(NAME.name));
+}
+
+export default useStore;
