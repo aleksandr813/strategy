af52180280360270753a45f00ce036916b47645a
sterio0o <ilyanaumov203@gmail.com>
2025-12-06 22:14:55 +0300

    Реализовал метод getUserArmies (#177)
    * Реализовал метод getUserArmies
    
    * Исправил лоигку возвращения армий, теперь возвращаются только армии, которые в данный момент находятся в пути
    
    * Исправил API.md

--- server/api/application/Application.php
+++ server/api/application/Application.php
@@ -275,4 +275,15 @@ class Application
         }
         return ['error' => 242];
     }
+
+    public function getUserArmies($params) {
+        if ($params['token']) {
+            $user = $this->user->getUser($params['token']);
+            if ($user) {
+                return $this->village->getUserArmies($user->id);
+            }
+            return ['error' => 705];
+        }
+        return ['error' => 242];
+    }
 }
