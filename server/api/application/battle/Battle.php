701aa9c501a33a1ef9b5fc721ca67893dc5fafc1
sterio0o <ilyanaumov203@gmail.com>
2025-12-18 22:08:02 +0300

    Реализовал логику определения каждым юнитом, ближайшего к себе врага и "отметка" его как потенциальную цель для атаки; изменил логику getUnits - теперь вместо поиска по userId будет совершен поиск по villageId (#207)

--- server/api/application/battle/Battle.php
+++ server/api/application/battle/Battle.php
@@ -24,4 +24,53 @@ class Battle {
 
         return true;
     }
+
+    public function unitsAttackDistance($userId) {
+        $alliedVillage = $this->db->getVillage($userId);
+        if (!$alliedVillage) {
+            return ['error' => 315];
+        }
+
+        $enemyVillage = $alliedVillage->attackId;
+
+        $alliedUnits = $this->db->getUnits($alliedVillage->id);
+        $enemyUnits = $this->db->getUnits($enemyVillage);
+
+        $result = [];
+
+        foreach($alliedUnits as $alliedUnit) {
+            if ($alliedUnit['onACrusade'] || $alliedUnit['currentHp'] <= 0) {
+                continue;
+            }
+
+            $attackRange = (int)$alliedUnit['rangeAttack'];
+            
+            $minDistance = PHP_INT_MAX;
+            $nearestEnemyUnit = null;
+
+            foreach($enemyUnits as $enemyUnit) {
+                if ($enemyUnit['onACrusade'] || $enemyUnit['currentHp'] <= 0) {
+                    continue;
+                }
+
+                $distance = $this->culculateDistance($alliedUnit['x'], $alliedUnit['y'], $enemyUnit['x'], $enemyUnit['y']);
+
+                if ($distance < $attackRange && $distance < $minDistance) {
+                    $nearestEnemyUnit = $enemyUnit['id'];
+                    $minDistance = $distance;
+                }
+            }
+
+            $result[] = [
+                'attackerId' => $alliedUnit['id'],
+                'nearestEnemyUnit' => $nearestEnemyUnit
+            ];
+        }
+
+        return $result;
+    }
+
+    private function culculateDistance($x1, $y1, $x2, $y2) {
+        return sqrt(pow($x2 - $x1, 2) + pow($y2 - $y1, 2));
+    }
 }
