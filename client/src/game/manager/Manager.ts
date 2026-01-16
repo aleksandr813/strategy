e6747126d82ad443e3e7179bd887837744dd6bb8
golpgelop <golpgelop@gmail.com>
2026-01-16 15:35:26 +0400

    modified:   client/src/game/manager/Manager.ts

--- client/src/game/manager/Manager.ts
+++ client/src/game/manager/Manager.ts
@@ -128,6 +128,7 @@ class Manager {
                     }
                 }
             });
+            
 
             if (movingUnits.length > 0 && this.currentServer) {
                 this.currentServer.moveUnits(movingUnits);
