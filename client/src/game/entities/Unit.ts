1d09d524a323582810d01b7d2861ffdc2a5ee81b
golpgelop <137231640+golpgelop@users.noreply.github.com>
2026-01-21 11:11:36 +0400

    скорость юнитов (#241)
    * modified:   client/src/game/entities/Unit.ts
    	modified:   client/src/game/manager/Manager.ts
    	modified:   client/src/services/server/Server.ts
    	modified:   client/src/services/server/types.ts
    
    * modified:   client/src/game/manager/Manager.ts
    
    * modified:   client/src/pages/Village/UI/UI.tsx
    
    ---------
    
    Co-authored-by: aleksander813 <molokoflina@gmail.com>

--- client/src/game/entities/Unit.ts
+++ client/src/game/entities/Unit.ts
@@ -16,6 +16,8 @@ export default class Unit {
     hp: number;
     maxHp: number;
     level: number;
+    speed: number = 1;
+    movementAccumulator: number = 0;
     sprites: number[];
     easystar: EasyStar.js;
     game: Game;
@@ -38,6 +40,7 @@ export default class Unit {
         this.hp = data.currentHp;
         this.maxHp = data.currentHp; 
         this.level = data.level;
+        this.speed = data.speed;
         this.unlockLevel = data.unlockLevel
         this.side = side;
 
@@ -61,6 +64,8 @@ export default class Unit {
 
     calcPath(destination: TPoint) {
         this.clearUnitMovement();
+
+        this.movementAccumulator = 0;
         
         const matrix = this.game.village.getMatrixForEasyStar(this);
         
