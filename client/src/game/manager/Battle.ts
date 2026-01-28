2545ac0a73bc1fd45fc712431a97f44b866d585c
aleksander813 <molokoflina@gmail.com>
2026-01-28 23:15:36 +0400

    modified:   client/src/game/manager/Battle.ts

--- client/src/game/manager/Battle.ts
+++ client/src/game/manager/Battle.ts
@@ -153,6 +153,7 @@ class Battle extends Manager {
             this.attackTarget(selectedUnits, targetUnit);
             return;
         }
+    
 
         const targetBuilding = this.game.getBuildings().find(b => {
             const [bx, by] = [b.coords[0].x, b.coords[0].y];
