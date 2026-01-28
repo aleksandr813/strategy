9b930c70740fc47eabd095c0b80b173095536102
aleksander813 <molokoflina@gmail.com>
2026-01-29 02:09:55 +0400

    modified:   client/src/pages/Village/UI/BuyBuildingsMenu/BuyBuildingsMenu.scss 	modified:   client/src/pages/Village/UI/BuyBuildingsMenu/BuyBuildingsMenu.tsx 	modified:   client/src/pages/Village/UI/BuyUnitsMenu/BuyUnitsMenu.scss 	modified:   client/src/pages/Village/UI/BuyUnitsMenu/BuyUnitsMenu.tsx 	modified:   client/src/pages/Village/Village.scss

--- client/src/pages/Village/UI/BuyBuildingsMenu/BuyBuildingsMenu.tsx
+++ client/src/pages/Village/UI/BuyBuildingsMenu/BuyBuildingsMenu.tsx
@@ -3,7 +3,6 @@ import GAMECONFIG from '../../../../game/gameConfig';
 import { GameContext } from '../../../../App';
 import { TBuildingType } from '../../../../services/server/types';
 import Server from '../../../../services/server/Server';
-import Button from '../../../../components/Button/Button';
 import { UIELEMENT, IBaseUIElement } from '../UI';
 import Store from '../../../../services/store/Store';
 import Mediator from '../../../../services/mediator/Mediator';
@@ -16,10 +15,9 @@ interface BuyBuildingMenuProps extends IBaseUIElement {
 }
 
 const BuyBuildingsMenu: React.FC<BuyBuildingMenuProps> = (props: BuyBuildingMenuProps) => {
-    const { setUIElement, store, mediator } = props;
+    const { setUIElement, store } = props;
     const gold = store.getMoney();
     
-
     const game = useContext(GameContext);
     const village = game.getVillage();
     const [buildingTypes, setBuildingTypes] = useState<TBuildingType[]>([]);
@@ -30,7 +28,6 @@ const BuyBuildingsMenu: React.FC<BuyBuildingMenuProps> = (props: BuyBuildingMenu
     const loadBuildingTypes = async (): Promise<TBuildingType[]> => {
         const server = new Server(game['store']);
         const types = await server.getBuildingTypes();
-
         const excludedTypes = GAMECONFIG.EXCLUDED_BUILDINGS;
     
         const filteredTypes = types.filter(type => 
@@ -66,7 +63,6 @@ const BuyBuildingsMenu: React.FC<BuyBuildingMenuProps> = (props: BuyBuildingMenu
     const isBuildingAvailable = (building: TBuildingType): boolean => {
         const currentGold = Number(gold);
         const buildingPrice = Number(building.priceLevel1);
-
         return townHallLevel >= building.unlockLevel && currentGold >= buildingPrice;
     }; 
 
@@ -80,40 +76,45 @@ const BuyBuildingsMenu: React.FC<BuyBuildingMenuProps> = (props: BuyBuildingMenu
     }, []);
 
     return (
-        <div>
-            <div className="buy-menu-overlay" onClick={closeBuyMenu}>
-                <div className="buy-menu-container" onClick={(e) => e.stopPropagation()}>
-                    <h3 className="buy-menu-title">
-                        Выберите здание
-                        <div className='money-indicator'>
-                            Монеты: {gold}
-                        </div>
-                    </h3>
+        <div className="buy-buildings-menu-overlay" onClick={closeBuyMenu}>
+            <div className="buy-buildings-menu-container" onClick={(e) => e.stopPropagation()}>
+                <h3 className="buy-buildings-menu-title">
+                    Выберите здание
+                    <div className='money-indicator'>
+                        Монеты: {gold}
+                    </div>
+                </h3>
 
-                    {
-                        buildingTypes.map((building) => (
-                            <div key={building.id} className={`buy-menu-ite`}>
-                                <div className="building-info">
-                                    <span className="building-name">{building.type}</span>
-                                    <span className="building-details">
-                                        HP: {building.hp} | Цена: {building.priceLevel1}
-                                    </span>
-                                </div>
-                                <Button
-                                    onClick={() => buyBuilding(building)}
-                                    text='Купить'
-                                    isDisabled={!isBuildingAvailable(building)}
-                                />
+                <div className="buy-buildings-items-list">
+                    {buildingTypes.map((building) => (
+                        <div 
+                            key={building.id} 
+                            className={`buy-buildings-menu-item ${!isBuildingAvailable(building) ? 'disabled' : ''}`}
+                        >
+                            <div className="building-info">
+                                <span className="building-name">{building.type}</span>
+                                <span className="building-details">
+                                    {building.unlockLevel > 0 && `Ур. Ратуши ${building.unlockLevel} `}<br/>
+                                    HP: {building.hpLevel1} <br/> Цена: {building.priceLevel1}
+                                </span>
                             </div>
-                        ))
-                    }
-
-                    <Button
-                        onClick={closeBuyMenu}
-                        text='Закрыть'
-                        className="buy-menu-close-button"
-                    />
+                            <button
+                                className="buy-buildings-menu-button"
+                                onClick={() => buyBuilding(building)}
+                                disabled={!isBuildingAvailable(building)}
+                            >
+                                Купить
+                            </button>
+                        </div>
+                    ))}
                 </div>
+
+                <button
+                    className="buy-buildings-menu-close-button"
+                    onClick={closeBuyMenu}
+                >
+                    Закрыть
+                </button>
             </div>
         </div>
     )
